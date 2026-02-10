09255b72c26784eefd62f4eb959de8543120dc26
agrikencanadata-creator <agrikencanadata@gmail.com>
2026-02-10 13:16:14 +0700

    Create proc_load_bronze.sql

---
+++ scripts/bronze/proc_load_bronze.sql
@@ -0,0 +1,160 @@
+-- THIS SCRIPT IS USED TO LOAD BRONZE LAYER AND IT WILL BE USED IN DAILY BASIS
+-- IT WILL BE RUN EVERYDAY IN ORDER TO GET NEW CONTENT TO DATA WAREHOUSE
+-- SO WE SHOULD CREATE STORED PROCEDURES FROM THE SCRIPT
+
+/*
+=======================================================================================
+Stored Procedure: Load Bronze Layer (Source -> Bronze)
+=======================================================================================
+Script Purpose:
+	This stored procedure loads data into the 'bronze' schema from external CSV files.
+	It performs the following actions:
+		- Truncates the bronze tables before loading data
+		- Bulk Inserts data from csv files into bronze tables
+
+Parameters:
+	None
+	This stored procedure does not accept any parameters or return any values
+
+Usage Example:
+	Exec bronze.load_bronze;
+=======================================================================================
+*/
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+	BEGIN TRY
+		SET @batch_start_time = GETDATE();
+		PRINT '=================================================';
+		PRINT 'Loading Bronze Layer';
+		PRINT '=================================================';
+
+		PRINT '-------------------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '-------------------------------------------------';
+		PRINT '';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_cust_info';
+		TRUNCATE TABLE bronze.crm_cust_info;
+
+		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\vinap\OneDrive\Documents\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second,@start_time,@end_time) as NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------';
+		PRINT '';
+
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_prd_info';
+		TRUNCATE TABLE bronze.crm_prd_info;
+
+		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\vinap\OneDrive\Documents\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second,@start_time,@end_time) as NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------';
+		PRINT '';
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.crm_sales_details';
+		TRUNCATE TABLE bronze.crm_sales_details;
+
+		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\vinap\OneDrive\Documents\Data with Baraa\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second,@start_time,@end_time) as NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------';
+		PRINT '';
+
+
+		PRINT '-------------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '-------------------------------------------------';
+		PRINT '';
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_loc_a101';
+		TRUNCATE TABLE bronze.erp_loc_a101;
+
+		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\vinap\OneDrive\Documents\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\loc_a101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second,@start_time,@end_time) as NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------';
+		PRINT '';
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_cust_az12';
+		TRUNCATE TABLE bronze.erp_cust_az12;
+
+		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\vinap\OneDrive\Documents\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\cust_az12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second,@start_time,@end_time) as NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------';
+		PRINT '';
+		
+		SET @start_time = GETDATE();
+		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\vinap\OneDrive\Documents\Data with Baraa\sql-data-warehouse-project\datasets\source_erp\px_cat_g1v2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+		SET @end_time = GETDATE();
+		PRINT '>> Load Duration: ' + CAST (DATEDIFF(second,@start_time,@end_time) as NVARCHAR) + ' seconds';
+		PRINT '>> ------------------------';
+		PRINT '';
+
+		SET @batch_end_time = GETDATE();
+		PRINT '====================================================';
+		PRINT 'Loading Bronze Layer is Completed';
+		PRINT '  - Total Load Duration: ' + CAST(DATEDIFF(SECOND,@batch_start_time,@batch_end_time) AS NVARCHAR) + ' seconds'; 
+		PRINT '====================================================';
+		PRINT '';
+	END TRY
+	BEGIN CATCH
+		PRINT '====================================================';
+		PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER';
+		PRINT 'ERROR MESSAGE' + ERROR_MESSAGE();
+		PRINT 'ERROR MESSAGE' + CAST (ERROR_NUMBER() AS NVARCHAR);
+		PRINT 'ERROR MESSAGE' + CAST (ERROR_STATE() AS NVARCHAR);
+		PRINT '====================================================';
+	END CATCH
+END
