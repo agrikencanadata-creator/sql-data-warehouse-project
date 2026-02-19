cb247896f28165e2d9888d39b8dda09f4d19df61
agrikencanadata-creator <agrikencanadata@gmail.com>
2026-02-19 10:39:49 +0700

    Update advance_analysis_checks.sql

--- tests/advance_analysis_checks.sql
+++ tests/advance_analysis_checks.sql
@@ -171,5 +171,25 @@ SELECT
 FROM yearly_product_sales
 ORDER BY product_name, order_year
 
+-- Part-to-whole Analysis
+-- Identify categories
+WITH category_sales AS
+(
+SELECT 
+	category,
+	SUM(sales_amount) AS total_sales
+FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON p.product_key = f.product_key
+GROUP BY category
+)
+
+SELECT
+category,
+total_sales,
+SUM(total_sales) OVER() AS overall_sales,
+CONCAT(ROUND(100 * (CAST (total_sales AS FLOAT) / SUM(total_sales) OVER()),2),'%') AS percenteage_of_total_sales
+FROM category_sales
+ORDER BY total_sales DESC
 
 
