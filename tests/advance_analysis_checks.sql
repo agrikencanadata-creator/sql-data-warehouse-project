08f5ea1caf429a5f6f322f396815f463b3a85dcb
agrikencanadata-creator <agrikencanadata@gmail.com>
2026-02-19 10:24:33 +0700

    Update advance_analysis_checks.sql

--- tests/advance_analysis_checks.sql
+++ tests/advance_analysis_checks.sql
@@ -91,3 +91,85 @@ FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY DATETRUNC(MONTH, order_date))t;
 GO
+
+-- Calculate the running total of sales over time which is partitioned by year
+-- The running total of sales will be summed up only within the same year over time
+
+-- monthly basis
+SELECT
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER (PARTITION BY YEAR(order_date) ORDER BY order_date) AS running_total_sales			-- Window Function
+	AVG(avg_price) OVER (PARTITION BY order_date ORDER BY order_date) AS moving_avg_price						-- Window Function
+FROM
+	(
+	SELECT
+		DATETRUNC(MONTH, order_date) AS order_month,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS avg_price
+	FROM gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+	)t;
+GO
+
+-- yearly basis
+SELECT
+	order_date,
+	total_sales,
+	SUM(total_sales) OVER (PARTITION BY YEAR(order_date) ORDER BY order_date) AS running_total_sales			-- Window Function
+	AVG(avg_price) OVER (PARTITION BY order_date ORDER BY order_date) AS moving_avg_price						-- Window Function
+FROM
+	(
+	SELECT
+		DATETRUNC(YEAR, order_date) AS order_year,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS avg_price
+	FROM gold.fact_sales
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(YEAR, order_date)
+	)t;
+GO
+
+/*
+Analyze the yearly performance of products by comparing their sales
+to both the average sales performance of the product and the previous year sales
+*/
+WITH yearly_product_sales AS
+(
+SELECT
+	YEAR (f.order_date) AS order_year,
+	p.product_name,
+	SUM (f.sales_amount) AS current_sales
+FROM gold.fact_sales f
+	LEFT JOIN gold.dim_products p
+	ON f.product_key = p.product_key
+WHERE f.order_date IS NOT NULL
+GROUP BY YEAR(f.order_date), p.product_name
+-- ORDER BY YEAR(f.order_date), p.product_name
+)
+
+SELECT
+	order_year,
+	product_name,
+	current_sales,
+	AVG(current_sales) OVER (PARTITION BY product_name) AS avg_sales,
+	current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg_sales,
+	CASE
+		 WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above avg'
+ 		 WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below avg'
+		 ELSE 'avg'
+	END AS avg_change,
+	-- Year-over-year Analysis
+	LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year ASC) AS py_sales,
+	current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year ASC) AS diff_py_sales,
+	CASE
+		 WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year ASC) > 0 THEN 'Increase'
+		 WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year ASC) < 0 THEN 'Decrease'
+		 ELSE 'No Change'	
+	END AS py_change
+FROM yearly_product_sales
+ORDER BY product_name, order_year
+
+
+
