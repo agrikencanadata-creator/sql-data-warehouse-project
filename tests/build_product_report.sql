84f4aeaef0ceae60131dba81a6e07314f5228c42
agrikencanadata-creator <agrikencanadata@gmail.com>
2026-02-20 16:29:47 +0700

    Update build_product_report.sql

--- tests/build_product_report.sql
+++ tests/build_product_report.sql
@@ -20,8 +20,11 @@ Highlights:
 		- average monthly revenue
 ================================================================================================
 */
+IF OBJECT_ID ('gold.report_products','V') IS NOT NULL
+	DROP VIEW gold.report_products;
+GO
 
-
+CREATE VIEW gold.report_products AS
 /*
 ------------------------------------------------------------------------------------------------
 1) Base Query: Retrieves core columns from tables
@@ -29,23 +32,23 @@ Highlights:
 */
 WITH base_query AS
 (
-SELECT
-	f.order_number,
-	f.order_date,
-	f.customer_key,
-	f.sales_amount,
-	f.quantity,
-	p.product_key,
-	p.product_number,
-	p.product_name,
-	p.category,
-	p.subcategory,
-	p.cost
-FROM gold.fact_sales f
-	LEFT JOIN gold.dim_products p
-	ON p.product_key = f.product_key
-WHERE order_date IS NOT NULL								-- Only consider valid sales dates
-)
+	SELECT
+		f.order_number,
+		f.order_date,
+		f.customer_key,
+		f.sales_amount,
+		f.quantity,
+		p.product_key,
+		p.product_number,
+		p.product_name,
+		p.category,
+		p.subcategory,
+		p.cost
+	FROM gold.fact_sales f
+		LEFT JOIN gold.dim_products p
+		ON p.product_key = f.product_key
+	WHERE order_date IS NOT NULL								-- Only consider valid sales dates
+),
 
 /*
 ------------------------------------------------------------------------------------------------
@@ -58,9 +61,60 @@ WHERE order_date IS NOT NULL								-- Only consider valid sales dates
 ------------------------------------------------------------------------------------------------
 */
 
+product_aggregation AS
+(
+	SELECT
+		product_key,
+		product_number,
+		product_name,
+		category,
+		subcategory,
+		cost,
+		DATEDIFF(MONTH,MIN(order_date), MAX(order_date)) AS lifespan,
+		MAX(order_date) AS last_sale_date,
+		COUNT(DISTINCT order_number) AS total_order,
+		COUNT(DISTINCT customer_key) AS total_customers,
+		SUM(sales_amount) AS total_sales,
+		SUM(quantity) AS total_quantity,
+		ROUND(AVG(CAST(sales_amount AS FLOAT) / NULLIF(quantity,0)),1) AS avg_selling_price
+	FROM base_query
+	GROUP BY product_key, product_number, product_name, category, subcategory, cost
+)
 
+/*
+------------------------------------------------------------------------------------------------
+2. Segments products by revenue to identify High-performers, Mid-range, or Low-performers.
+------------------------------------------------------------------------------------------------
+*/
 
 SELECT
-	p.product_key,
-	p.product_number,
-	p.product_name,
+	product_key,
+	product_number,
+	product_name,
+	category,
+	subcategory,
+	cost,
+	lifespan,
+	last_sale_date,
+	DATEDIFF(MONTH,last_sale_date,GETDATE()) AS recency_in_months,
+	CASE
+		 WHEN total_sales > 50000 THEN 'High-performers'
+		 WHEN total_sales >= 10000 THEN 'Mid-range'
+		 ELSE 'Low-performers'
+	END AS product_segment,
+	total_order,
+	total_customers,
+	total_sales,
+	total_quantity,
+	avg_selling_price,
+	-- Average Order Revenue (AOR)
+	CASE
+		 WHEN total_order = 0 THEN 0
+		 ELSE total_sales / total_order
+	END AS avg_order_revenue,
+	-- Average Monthly Revenue 
+	CASE
+		 WHEN lifespan = 0 THEN total_sales
+		 ELSE total_sales / lifespan
+	END AS avg_monthly_revenue
+FROM product_aggregation
